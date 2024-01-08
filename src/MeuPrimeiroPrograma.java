768dae783c0abc36aa88ff31a711ca148f462208
halof <halofmayor@gmail.com>
2024-01-08 02:37:55 +0000

    Initial commit

---
+++ src/MeuPrimeiroPrograma.java
@@ -0,0 +1,11 @@
+public class MeuPrimeiroPrograma {
+
+    //O projeto java procura a função main (ponto de entrada/entry ppint) para iniciar o projeto
+    public static void main(String[] args) {
+        //Os dados são armazenados em propriedades/variaveis
+        //Metodos/funcoes sao instrucoes (bloco de codigos) reutilizaveis para alterar dados
+        System.out.println("Hello World");
+        System.out.println(6+7);
+    }
+
+}
